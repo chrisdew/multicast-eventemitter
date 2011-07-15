8d50da1c79470dd779833623682fd9c5f51cbd65
Chris Dew <cmsdew@gmail.com>
2011-07-15 11:12:23 +0100

    added overrides option

--- examples/loopback.js
+++ examples/loopback.js
@@ -4,7 +4,16 @@ var mee = require('../lib/multicast-eventemitter');
 
 var emitter = mee.getEmitter();
 
+// emit a packet everu second, 
+setInterval(function() {
+  console.log('emitting channelA');
+  emitter.emit('channelA', 'this is channel A', new Date().getTime());
+  console.log('emitting channelB');
+  emitter.emit('channelB', 'this is channel B', new Date().getTime());
+}, 1000);
+
 // subscribe to channelA events
 emitter.on('channelA', function(text, time) {
   console.log('message received on channelA:', text, time);
 });
+
--- examples/sender.js
+++ examples/sender.js
@@ -2,10 +2,12 @@
 
 var mee = require('../lib/multicast-eventemitter');
 
-var emitter = new mee.MulticastEventEmitter();
+var emitter = mee.getEmitter();
 
 // emit a packet everu second, 
 setInterval(function() {
+  console.log('emitting channelA');
   emitter.emit('channelA', 'this is channel A', new Date().getTime());
+  console.log('emitting channelB');
   emitter.emit('channelB', 'this is channel B', new Date().getTime());
 }, 1000);
